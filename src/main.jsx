b2791efdf8df68375e870f46ff0ef0157edd7e32
Neha Madupalli <nehamadupalli4@gmail.com>
2026-06-09 10:07:53 +0530

    fix admin import issue

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./index.css";
 import App from "./App";
-import Admin from "./Admin";
+import Admin from "./admin";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
